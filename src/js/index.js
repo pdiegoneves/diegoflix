5ad1c1f7eee516b75a1d01a72291ca7ff9d3eae9
Diego Neves <pdcassiano@gmail.com>
2022-12-05 06:13:59 -0300

    feat: iniciando a montagem do hero

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+const API_KEY = 'a8917691c1e73fa663f848a0e1f2e239'
+const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=1&language=pt-BR`
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?&api_key=${API_KEY}&language=pt-BR&query="`
+
+async function getMovies(url) {
+  const res = await fetch(url)
+  const data = await res.json()
+  mountHero(data.results)
+}
+
+getMovies(API_URL)
+
+function mountHero(movies) {
+    const hero = document.getElementById('hero')
+    hero.style.background = `${IMG_PATH}{movies[0].poster_path}`
+    hero.addEventListener('click', () => alert('teste'))
+    console.log(IMG_PATH + movies[0].poster_path)
+}
